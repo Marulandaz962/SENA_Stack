84e574cf143441655237bc8f08d1d4481058f6ba
JORGE ANDRES FERNANDEZ PRADA <jafernandez809@misena.edu.co>
2023-10-27 19:02:04 -0500

    entrega boton crear

--- senaback/static/senaback/js/index.js
+++ senaback/static/senaback/js/index.js
@@ -51,28 +51,7 @@ const listConsumables = async () => {
 
 					<button type="button" id="editar-elemento" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
 					Launch static backdrop modal
-					</button>
-					<div class="modal" tabindex="-1">
-					<div class="modal-dialog">
-					  <div class="modal-content">
-						<div class="modal-header">
-						  <h5 class="modal-title">Editar Elemento</h5>
-						  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-						</div>
-						<div class="modal-body">
-						  <p>Modal body text goes here.</p>
-						</div>
-						<div class="modal-footer">
-						  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-						  <button type="button" class="btn btn-primary">Save changes</button>
-						</div>
-					  </div>
-					</div>
-				  </div>
-
-
-				
-					
+					</button>					
 					</td>
 				</tr>`;
 		});
--- senaback/views.py
+++ senaback/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http.response import JsonResponse
-from .models import ElementoConsumible
 from .forms import ElementoConsumibleForm
-from .models import Usuario
-from .models import entrega
-from .models import ElementoDevolutivo
+from .models import *
 from .forms import ElementoDevolutivoForm
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
@@ -166,7 +163,7 @@ def obtener_entregas(request):
             'cantidad': entrega.cantidad_total,
             'responsable': entrega.valor,
         })
-    return render(request, 'senaback/index_entrega.html', {'entregas': entregas})
+    return render(request, 'senaback/index_entregas.html.html', {'entregas': entregas})
 
 def obtener_entregas(request):
     entregas = entrega.objects.all()
@@ -179,7 +176,7 @@ def obtener_entregas(request):
             'cantidad': entrega.cantidad_total,
             'responsable': entrega.valor,
         })
-    return render(request, 'senaback/index_entrega.html', {'entregas': entregas})
+    return render(request, 'senaback/index_entregas.html.html', {'entregas': entregas})
 
 def getlist_entregas(request):
     entregas = list(entrega.objects.values())
@@ -189,10 +186,10 @@ def getlist_entregas(request):
 def list_entregas(request):
     entregas = entrega.objects.all()
     data = {'entregas':entregas}
-    return render(request, 'senaback/index_entrega.html', data)
+    return render(request, 'senaback/index_entregas.html.html', data)
 
 def crear_entrega(request):
-    return render(request, 'senaback/index_entrega.html')
+    return render(request, 'senaback/index_entregas.html.html')
 
 # Usuarios
 def crear_entrega(request):
